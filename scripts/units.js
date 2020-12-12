3380a4ba375e23719f02b00c74c1dbafaf767b81
EmanuelG-Gaming <68462759+EmanuelG-Gaming@users.noreply.github.com>
2020-12-12 18:45:56 +0200

    Update units.js

--- scripts/units.js
+++ scripts/units.js
@@ -1,5 +1,8 @@
 const excel = extendContent(UnitType, "excelsus", {});
 const excel2 = extendContent(UnitType, "excelsus2", {});
+const emper = extendContent(UnitType, "emperor", {});
+emper.abilities.add(
+	UnitSpawnAbility(UnitTypes.flare, 400, 65, -50)
 );
 excel.constructor = () => {
     const unit = extend(UnitEntity, {});
@@ -9,3 +12,8 @@ excel2.constructor = () => {
     const unit = extend(UnitEntity, {});
     return unit;
 };
+emper.constructor = () => {
+    const unit = extend(UnitEntity, {});
+    return unit;
+};
+
