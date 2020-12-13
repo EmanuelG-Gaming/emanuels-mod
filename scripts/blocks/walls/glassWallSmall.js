9a4fb1367d667839c91d95a96dc1c5637bcedc0f
EmanuelG-Gaming <68462759+EmanuelG-Gaming@users.noreply.github.com>
2020-12-13 09:44:27 +0200

    Delete glassWallSmall.js

--- scripts/blocks/walls/glassWallSmall.js
+++
@@ -1,7 +0,0 @@
-const wallEffect = new Effect(60, e => {
-    Draw.color(Pal.accent, Color.white, e.fslope());
-    Draw.alpha(e.fout() * 0.5)
-    Lines.stroke(2 * e.fslope());
-    Draw.blend(Blending.additive);
-    Lines.square(e.x, e.y, 5, e.fout());
-    Draw.blend();
