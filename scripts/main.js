0ef8b8ea5ff13a04796957bbec93bb1cf5f890b5
EmanuelG-Gaming <68462759+EmanuelG-Gaming@users.noreply.github.com>
2020-12-13 19:41:15 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,2 @@
 require('units');
 require('blocks');
-require('main);
-
-const omegaTerro = extendContent(UnitType, "omegaTerror", {});
-omegaTerro.constructor = () => extend(LegsUnit, {
-  killed(){
-    print("you are like an ant trying to carry the whole universe!!!");
-    this.dead = false;
-  } 
-const terro = extendContent(UnitType, "terror", {});
-terro.constructor = () => extend(FlyingUnit, {
-  killed(){
-    this.super$killed();
-    print("[red]youre gonna pay for this!!!");
-    omegaTerro.spawn( Team.crux, this.x, this.y);
-  }
-});
