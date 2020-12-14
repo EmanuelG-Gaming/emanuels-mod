ee78f4351ea7466190676ff770c92147af1b7d34
EmanuelG-Gaming <68462759+EmanuelG-Gaming@users.noreply.github.com>
2020-12-14 20:37:11 +0200

    Update bigLancer.js

--- scripts/blocks/turret/bigLancer.js
+++ scripts/blocks/turret/bigLancer.js
@@ -28,3 +28,45 @@ const bigLacerShoot = new Effect(30, e => {
 	Lines.circle(e.x, e.y, e.fin() * 50);
 	Draw.color();
 });
+
+const smallLancerbul = extend(ArtilleryBulletType, {
+	load(){	
+        this.backRegion = Core.atlas.find("vanilla-upgraded-bullet-explo-back");
+    	this.frontRegion = Core.atlas.find("vanilla-upgraded-bullet-explo"); 	
+    },
+	draw(b){
+		Draw.color(Color.blue, Color.white, b.fin());
+		Fill.circle(b.x, b.y, 3);
+		Draw.color();
+	},
+	update(b){
+		Effect.create(Fx.none, b.x, b.y, 0, Color.white, null);
+	},
+	hit(b, hitx, hity){
+		const vec = new Vec2();
+		if(hitx != null && hity != null){
+			Effect.create(this.hitEffect, hitx, hity, 0, Color.white, null);
+			Sounds.spark.at(b);
+			for (var i = 0; i <= 2; i++){
+				Lightning.create(b.team, Color.blue, 15, b.x, b.y, b.rotation() + Mathf.range(46.0), Mathf.random(5, 16));
+			}
+		}
+	},
+	despawned(b){
+		for (var i = 0; i <= 2; i++){
+			const vec = new Vec2();
+			Sounds.spark.at(b);
+			Lightning.create(b.team, Color.blue, 15, b.x, b.y, b.rotation() + Mathf.range(46.0), Mathf.random(5, 16));
+		}
+	}
+});
+smallLancerbul.damage = 120;
+smallLancerbul.lifetime = 250;
+smallLancerbul.keepVelocity = false;
+smallLancerbul.hitEffect = Fx.flakExplosion;
+smallLancerbul.frontColor = Color.blue;
+smallLancerbul.backColor = Color.valueOf("00008bff");
+smallLancerbul.speed = 4;
+smallLancerbul.despawnedEffect = Fx.flakExplosion;
+smallLancerbul.drag = 0.042;
+smallLancerbul.pierce = false;
